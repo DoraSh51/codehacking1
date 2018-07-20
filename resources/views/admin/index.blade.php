56804d3fb55d3f571e4ad8d44ea7dfbc820c527c
Dora Shoninska <dorash@gmail.com>
2018-07-20 17:03:05 +0300

    Admin 1 commit

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,8 +1,14 @@
-<?php
+@extends('layouts.admin')
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+@section('content')
+
+    <h1>Admin</h1>
+    
+@endsection
+
+@section('footer')
+
+    <h4>footer Admin</h4>
+    
+@endsection
 
